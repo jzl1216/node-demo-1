b1fc98ed37cbb5f6b10250d794217316129111cb
jzl1216 <59465656+jzl1216@users.noreply.github.com>
2020-11-10 20:31:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-
+  
   if(path === '/'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.statusCode = 299
+    response.setHeader('Content-Type', 'text/html;charset=utf-8');
     response.write(`
         <!DOCTYPE HTML>
         <head>
@@ -35,12 +35,20 @@ var server = http.createServer(function(request, response){
         </body>
     `)
     response.end()
-  } else if(path === '/x'){
+  }
+    else if(path === '/x'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`body{color: red;}`)
     response.end()
-  } else {
+  } 
+    else if(path === '/y'){
+        response.statusCode = 200;
+        response.setHeader("Content-Type","text/javascript;charset=utf-8");
+        response.write(`console.log('这是JS内容')`);
+        response.end();
+    }
+  else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`你输入的路径不存在对应的内容`)
